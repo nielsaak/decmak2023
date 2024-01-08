5b51c27e1e9a5937480d31b7528955d18d55d35a
alekswael <amw98@outlook.dk>
2024-01-08 18:37:18 +0100

    recov work

--- parameter_recovery/hierarchical_parameter_recovery/recov_plot.R
+++ parameter_recovery/hierarchical_parameter_recovery/recov_plot.R
@@ -12,6 +12,7 @@ recov_plot <- function(true, infer, plot_lab_1, plot_lab_2, plot_col, title) {
     #scale_linetype(name="perfect recovery") +
     geom_abline(intercept=0, slope=1, linetype=2) +
     geom_smooth(method = "lm", se = T, formula = "y ~ x") +
+    stat_cor(method = "pearson", aes(label = ..r.label..), label) +
     theme_minimal() + #Setting theme
     xlab(plot_lab_1) + #Setting x label
     ylab(plot_lab_2) + #Setting y label
@@ -22,3 +23,21 @@ recov_plot <- function(true, infer, plot_lab_1, plot_lab_2, plot_col, title) {
   return(pl)
   
 }
+
+# # Load ggplot2
+# library(ggplot2)
+# 
+# # Your data
+# data <- data.frame(x = ..., y = ...)
+# 
+# # Linear regression
+# model <- lm(y ~ x, data = data)
+# 
+# # Get R-squared value
+# r_squared <- summary(model)$r.squared
+# 
+# # Create the plot
+# ggplot(data, aes(x = x, y = y)) +
+#   geom_point() +
+#   geom_abline(slope = coef(model)[2], intercept = coef(model)[1]) +
+#   annotate("text", x = your_x, y = your_y, label = paste("R^2 =", round(r_squared, 2)), parse = TRUE)
